51f4413c3c24be112c061f783c65a0731a6445a5
hikarizen-dev <hikarizen.gg@gmail.com>
2025-11-05 20:49:19 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
-// app/page.tsx o pages/index.tsx
-"use client";
-import React from "react";
+import type { NextConfig } from "next";
 
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-neutral-900 text-white flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">🎧 HiMusic</h1>
-      <p className="text-gray-400">Tu reproductor de música Hi-Res minimalista.</p>
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["http://localhost:3000"],
+};
 
-      <div className="mt-10 bg-neutral-800 p-6 rounded-2xl shadow-lg w-96 text-center">
-        <p className="mb-3">Reproduce, descarga y disfruta de calidad Hi-Res.</p>
-        <button className="bg-blue-500 hover:bg-blue-600 px-5 py-2 rounded-full transition">
-          Iniciar reproductor
-        </button>
-      </div>
-    </main>
-  );
-}
+export default nextConfig;
